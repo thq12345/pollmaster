93caf703d7f9286f23a6ee7168a72f3dd19ae17b
Fengrui Gan <fengruigan@gmail.com>
2021-11-18 13:52:21 -0800

    move styles into components

--- frontend/src/components/toastMessage.jsx
+++ frontend/src/components/toastMessage.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import { Toast, ToastContainer } from "react-bootstrap";
-import "../stylesheets/toastMessage.css";
+// import "../stylesheets/toastMessage.css";
 
 const ToastMessage = ({ show, message, setMessage, type, delay }) => {
+  const styles = {
+    successIcon: {
+      color: "rgb(62, 207, 62)",
+      fontSize: "1.5em",
+    },
+  };
+
   let [showToast, setShowToast] = useState(show);
 
   return (
@@ -17,7 +24,7 @@ const ToastMessage = ({ show, message, setMessage, type, delay }) => {
         autohide
       >
         <Toast.Header>
-          <i className={`fas fa-check-square ${type.toLowerCase()}`}></i>
+          <i className="fas fa-check-square" style={styles.successIcon}></i>
           <strong className="me-auto ms-2">{type || "Success"}</strong>
         </Toast.Header>
         <Toast.Body>{message}</Toast.Body>
